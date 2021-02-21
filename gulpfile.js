1d14454206882b66d3ce039aa70ee8d9b2f4ab79
Bruno Sulawesi <brunocq1996@gmail.com>
2021-02-21 12:51:34 +0000

    Instalacion de jsdoc y eslint

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
- 
+var jsdoc = require('gulp-jsdoc3');  
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
@@ -26,6 +26,30 @@ gulp.task('serve', gulp.series(['sass'], function() {
   gulp.watch("./css/*.scss", gulp.series(['sass']));
   gulp.watch("./index.html").on('change', browserSync.reload);
   gulp.watch("./js/*.js").on('change', browserSync.reload);
+  gulp.watch("./js/*.js", gulp.series(['doc']));
+  gulp.watch("./js/*.js", gulp.series(['lint']));
 }));
+//JSDOC 3
+gulp.task('doc', function (cb) {
+  gulp.src(['./js/*.js'], {read: false})
+      .pipe(jsdoc(cb));
+});
+
+//ESLINT
+
+const eslint = require('gulp-eslint');
+
+gulp.task('lint', () => {
+  return src(['./js/*.js'])
+      // eslint() attaches the lint output to the "eslint" property
+      // of the file object so it can be used by other modules.
+      .pipe(eslint())
+      // eslint.format() outputs the lint results to the console.
+      // Alternatively use eslint.formatEach() (see Docs).
+      .pipe(eslint.format())
+      // To have the process exit with an error code (1) on
+      // lint error, return the stream and pipe to failAfterError last.
+      .pipe(eslint.failAfterError());
+});
 
 gulp.task('default', gulp.series(['serve']));
